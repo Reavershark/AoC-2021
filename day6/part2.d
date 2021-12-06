0077cbc4dc4596d69927bc14249f110798d2f198
Reavershark <jonasty1999@gmail.com>
2021-12-06 15:14:07 +0100

    Day 6

---
+++ day6/part2.d
@@ -0,0 +1,33 @@
+import std.stdio;
+import std.conv : to;
+import std.string : chomp;
+
+import std.array : split;
+import std.algorithm : map, splitter, sum;
+
+void shiftLeft(T)(T[] arr)
+in (arr.length >= 2)
+{
+  T temp = arr[0];
+  foreach (i; 1 .. arr.length)
+    arr[i - 1] = arr[i];
+  arr[$ - 1] = temp;
+}
+
+void main()
+{
+  alias T = long;
+  T[9] fish;
+
+  foreach (i; readln.chomp.split(",").map!(to!T))
+    fish[i]++;
+
+  foreach (day; 0 .. 256)
+  {
+    T parents = fish[0];
+    fish.shiftLeft;
+    fish[6] += parents;
+  }
+
+  writeln(fish[].sum);
+}
