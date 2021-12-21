f95bd7d4c2332667bb3eec8c015e9f71a79becc3
Jonas Meeuws <jonas.meeuws@gmail.com>
2021-12-22 00:01:49 +0100

    Day 21 part 2 wip

--- day21/part2.d
+++ day21/part2.d
@@ -1,170 +1,57 @@
-import std.stdio;
-import std.string : chomp;
+import std;
 
-import std.algorithm : min, max, count;
-
-class Tape
+long[10] universeFreq()
 {
-  static immutable long length = 512;
-  bool[] arr;
+  static long[10] freq;
+  static bool done;
 
-  this(string str)
-  in (str.length == length)
+  if (!done)
   {
-    arr.length = length;
-    foreach (i, c; str)
-      arr[i] = c == '#';
+    synchronized
+    {
+      foreach (a; [1, 2, 3])
+        foreach (b; [1, 2, 3])
+          foreach (c; [1, 2, 3])
+            freq[a + b + c]++;
+      done = true;
+    }
   }
-
-  bool opIndex(size_t i) const
-  in (i < 512)
-  {
-    return arr[i];
-  }
+  return freq;
 }
 
-struct Point
+/** 
+ * Returns: Times player1 won
+ */
+long turn(long[2] p, long[2] score, long turnI, long diceRoll, long universes)
 {
-  int x, y;
+  p[turnI] += diceRoll;
+  while (p[turnI] > 10)
+    p[turnI] -= 10;
 
-  // size_t toHash() const @nogc @safe pure nothrow
-  // {
-  //   return (x << int.sizeof) | y;
-  // }
+  score[turnI] += p[turnI];
+  if (score[turnI] >= 21)
+    return universes;
 
-  // bool opEquals(const Point other) const
-  // {
-  //   return x == other.x && y == other.y;
-  // }
-}
+  turnI = (turnI + 1) % 2;
 
-class Image
-{
-  bool[Point] map; // true = 1, false = 0, not present = infinity
-  bool infinity = false;
-
-  private this()
-  {
-  }
-
-  /// String ctor
-  this(Lines)(Lines lines)
-  {
-    int y;
-    foreach (line; lines)
-    {
-      int x;
-      foreach (c; line)
-      {
-        map[Point(x, y)] = c == '#';
-        x++;
-      }
-      y++;
-    }
-  }
-
-  bool opIndex(Point p) const
-  {
-    if (p in map)
-      return map[p];
-    else
-      return infinity;
-  }
-
-  bool opIndexAssign(bool value, Point p)
-  {
-    map[p] = value;
-    return value;
-  }
-
-  long countOnes() const
-  {
-    return map.byValue.count(true);
-  }
-
-  Point topLeft() const
-  {
-    Point result;
-    foreach (p; map.byKey)
-    {
-      result.x = min(result.x, p.x);
-      result.y = min(result.y, p.y);
-    }
-    return result;
-  }
-
-  Point bottomRight() const
-  {
-    Point result;
-    foreach (p; map.byKey)
-    {
-      result.x = max(result.x, p.x);
-      result.y = max(result.y, p.y);
-    }
-    return result;
-  }
-
-  void print() const
-  {
-    Point begin = topLeft;
-    Point end = bottomRight;
-
-    foreach (y; begin.y - 3 .. end.y + 4)
-    {
-      foreach (x; begin.x - 3 .. end.x + 4)
-      {
-        Point p = Point(x, y);
-        write(this[p] ? '#' : '.');
-      }
-      writeln;
-    }
-  }
-
-  long tapeIndex(Point p) const
-  {
-    long result;
-    foreach (y; [p.y - 1, p.y, p.y + 1])
-      foreach (x; [p.x - 1, p.x, p.x + 1])
-      {
-        result <<= 1;
-        result |= this[Point(x, y)];
-      }
-    return result;
-  }
-
-  Image enhanced(Tape tape) const
-  {
-    Image result = new Image;
-
-    if (!infinity && tape[0])
-      result.infinity = true;
-
-    Point begin = topLeft;
-    Point end = bottomRight;
-
-    foreach (y; begin.y - 3 .. end.y + 4)
-      foreach (x; begin.x - 3 .. end.x + 4)
-      {
-        Point p = Point(x, y);
-        result[p] = tape[tapeIndex(p)];
-      }
-
-    return result;
-  }
+  long timesP1Won;
+  foreach (i, v; universeFreq.parallel)
+    if (i > 0)
+      timesP1Won += turn(p, score, turnI, i, universes + v);
+  return timesP1Won;
 }
 
 void main()
 {
-  Tape tape = new Tape(readln.chomp);
-  readln;
-  Image image = new Image(stdin.byLine);
+  long[2] p = [4, 8]; // Example
+  // long[2] p = [2, 1]; // Input
 
-  long steps = 50;
-  foreach (i; 0 .. steps)
-  {
-    writefln!"Step %d/%d"(i + 1, steps);
-    image = image.enhanced(tape);
-  }
+  long[2] score;
 
-  image.countOnes.writeln;
+  long timesP1Won;
+  foreach (i, v; universeFreq)
+    if (i > 0)
+      timesP1Won += turn(p, score, 0, i, v);
+
+  writeln(timesP1Won);
 }
